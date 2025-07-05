1f7c063fcf2702d7c55015b4ad194445f1838473
pouriya-3a <pooryasaboori1385@gmail.com>
2025-07-05 22:07:41 +0930

    fixing links and Done Coding ...

--- src/Components/BottomNav/index.jsx
+++ src/Components/BottomNav/index.jsx
@@ -1,12 +1,14 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import { authContext } from '../../Utils/AuthContext'
 
 export default function BottomNav() {
+  const {token , handleToken} =useContext(authContext)
   return (
     <div className='py-3 z-10 bg-white shadow rounded-t-3xl fixed bottom-0 left-0 right-0 shadow-gray-600 md:hidden'>
       <ul className="flex px-[1%] justify-between w-[100%]">
           <li className=" hover:bg-gray-100 rounded-1xl py-2 px-3 font-bold text-[var(--lowBlack)]">
-            <Link to="/">بازی</Link>
+            <Link to="/games">بازی</Link>
           </li>
           <li className=" hover:bg-gray-100 rounded-1xl py-2 px-3 font-bold text-[var(--lowBlack)]">
             <Link to="/apps">برنامه</Link>
@@ -18,7 +20,7 @@ export default function BottomNav() {
             <Link to="/">سریال</Link>
           </li>
           <li className=" hover:bg-gray-100 rounded-1xl py-2 px-3 font-bold text-[var(--lowBlack)]">
-            <Link to="/">توسعه دهندگان</Link>
+            {!token ? <Link to="/login">توسعه دهندگان</Link> : <button onClick={()=>handleToken(null)} className="text-red-600">خارج شوید !</button>}
           </li>
         </ul>
     </div>
